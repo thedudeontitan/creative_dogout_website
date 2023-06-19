a42894bc5d56195d31672f504da61e117d2ee6be
Ankit Kokane <97180942+thedudeontitan@users.noreply.github.com>
2023-06-19 22:54:06 +0530

    added contact page

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,4 +1,5 @@
 import Image from "next/image";
+import Link from "next/link";
 import React from "react";
 import { BsArrowRight } from "react-icons/bs";
 
@@ -8,76 +9,84 @@ const team_info = [
     name: "John Doe",
     position: "Position",
     image: "/pfp.png",
+    linkedin: "https://www.linkedin.com/in/ankit-kokane-795162197/",
   },
   {
     key: "2",
     name: "John Doe",
     position: "position",
     image: "/pfp.png",
+    linkedin: "https://www.linkedin.com/in/ankit-kokane-795162197/",
   },
   {
     key: "3",
     name: "John Doe",
     position: "position",
     image: "/pfp.png",
+    linkedin: "https://www.linkedin.com/in/ankit-kokane-795162197/",
   },
   {
     key: "4",
     name: "John Doe",
     position: "position",
     image: "/pfp.png",
+    linkedin: "https://www.linkedin.com/in/ankit-kokane-795162197/",
   },
   {
     key: "5",
     name: "John Doe",
     position: "position",
     image: "/pfp.png",
+    linkedin: "https://www.linkedin.com/in/ankit-kokane-795162197/",
   },
   {
     key: "6",
     name: "John Doe",
     position: "position",
     image: "/pfp.png",
+    linkedin: "https://www.linkedin.com/in/ankit-kokane-795162197/",
   },
   {
     key: "7",
     name: "John Doe",
     position: "position",
     image: "/pfp.png",
+    linkedin: "https://www.linkedin.com/in/ankit-kokane-795162197/",
   },
   {
     key: "8",
     name: "John Doe",
     position: "position",
     image: "/pfp.png",
+    linkedin: "https://www.linkedin.com/in/ankit-kokane-795162197/",
   },
 ];
 
 function page() {
   return (
     <section className="">
-      <div className="flex flex-col border-x mx-2 border-black min-h-screen pr-10">
+      <div className="flex flex-col border-x mx-2 border-black min-h-screen ">
         <div className="mx-auto flex flex-col h-screen justify-center w-full">
           <div className="flex flex-col">
-            <span className="lg:ml-96 mt-20 text-5xl font-medium">
+            <span className="lg:ml-96 mt-20 text-5xl font-medium lg:pr-10 px-2 lg:px-0">
               Stimulating growth for ambitious brands.
             </span>
-            <div className="border-b border-black lg:mt-6 lg:mb-20" />
-            <span className="lg:ml-96 text-2xl font-normal ">
+            <div className=" border-b border-black lg:mt-6 lg:mb-20" />
+            <span className="lg:ml-96 text-2xl font-normal lg:pr-10 px-2 lg:px-0">
               We deliver inspirational brand experiences. Together, let’s unlock
               growth!
             </span>
-            <span className="lg:ml-96 text-2xl font-normal lg:mt-10">
+            <span className="lg:ml-96 text-2xl font-normal lg:mt-10 lg:pr-10 px-2 lg:px-0">
               Together, let’s unlock growth!
             </span>
-            <button className="lg:ml-96 text-lg font-normal lg:mt-10 border border-black mx-auto px-4 py-2 flex flex-row gap-x-2 items-center">
+            <button className="lg:ml-96 text-lg font-normal lg:mt-10 border border-black mx-auto px-4 py-2 flex flex-row gap-x-2 items-center hover:scale-[1.02] transition-all duration-500">
               Let's connect <BsArrowRight />
             </button>
           </div>
         </div>
       </div>
       <div className="bg-purple-950 h-[80vh]"></div>
-      <div className="flex flex-col border-x mx-2 border-black min-h-screen gap-y-8 pr-10">
+      <div className="flex flex-col border-x mx-2 border-black min-h-screen gap-y-8 lg:pr-10 px-2 lg:px-0">
         <span className="lg:text-4xl lg:ml-96 font-medium mt-20 mb-10">
           Building through Purpose driven Communications.
         </span>
@@ -103,39 +112,44 @@ function page() {
           With Creative Dugout, you get a partner to operate, innovate and make
           winning brand experiences.{" "}
         </span>
-        <button className="lg:ml-96 text-lg font-normal lg:mt-5 border border-black mx-auto px-4 py-2 flex flex-row gap-x-2 items-center">
+        <button className="lg:ml-96 text-lg font-normal lg:mt-5 border border-black mx-auto px-4 py-2 flex flex-row gap-x-2 items-center hover:scale-[1.02] transition-all duration-500">
           Know our expertise <BsArrowRight />
         </button>
       </div>
       <div className="bg-purple-950 h-[80vh]"></div>
-      <div className="flex flex-col border-x mx-2 border-black  gap-y-8 pr-10">
-        <span className="lg:text-3xl font-medium lg:mt-20 lg:ml-96">
+      <div className="flex flex-col border-x mx-2 border-black gap-y-8 ">
+        <span className="lg:text-3xl font-medium lg:mt-20 lg:ml-96 lg:pr-10 px-2 lg:px-0">
           At Creative Dugout, our work principles act as a layer of protection
           for our clients.
         </span>
-        <span className="text-2xl lg:ml-96">
+        <span className="lg:text-2xl lg:ml-96 lg:pr-10 px-2 lg:px-0">
           We deeply respect and care about our people and clients. We are
           committed to growth, retention, honesty and fair dealing. We align our
           expertise with stakeholders’ interests. We are dedicated to building
           long-term rewarding relationships with clients.
         </span>
+        <div className="border-b border-black lg:mt-6 lg:mb-20" />
       </div>
-      <div className="flex flex-col border-x mx-2 border-black min-h-screen gap-y-8 pr-10">
+      <div className="flex flex-col border-x mx-2 border-black min-h-screen gap-y-8 lg:pr-10 px-2 lg:px-0">
         <span className="lg:text-4xl lg:ml-96 mt-20">Our People.</span>
         <span className="lg:text-2xl lg:ml-96">
           We have some curious, creative thinkers and problem solvers to
           cultivate your brand growth. Expect true commitment at every step.
         </span>
         <div className="lg:ml-96 mt-20">
-          <div className="grid grid-cols-2 gap-4 ">
+          <div className="grid grid-cols-1 lg:grid-cols-2 gap-4">
             {team_info.map((info) => (
-              <div className="flex flex-row" key={info.key}>
+              <Link
+                href={info.linkedin}
+                className="flex flex-row"
+                key={info.key}
+              >
                 <Image src={info.image} alt="" width={80} height={10} />
                 <div className="flex flex-col px-5">
                   <span className="text-2xl text-gray-600">{info.name}</span>
                   <span className="text-xl text-gray-600">{info.position}</span>
                 </div>
-              </div>
+              </Link>
             ))}
           </div>
         </div>
---
+++ app/contact/page.tsx
@@ -0,0 +1,65 @@
+import Link from "next/link";
+import React from "react";
+
+function page() {
+  return (
+    <section className="">
+      <div className="flex flex-col lg:flex-row border-x mx-2 min-h-screen border-black pr-10">
+        <form className="lg:ml-20 flex flex-col gap-5 lg:mt-60 items-start">
+          <input
+            className="p-2 border"
+            type="text"
+            placeholder="Name"
+            required={true}
+          />
+          <input
+            className="p-2 border"
+            type="text"
+            placeholder="Company"
+            required={true}
+          />
+          <input
+            className="p-2 border"
+            type="text"
+            placeholder="Email"
+            required={true}
+          />
+          <input
+            className="p-2 border"
+            type="text"
+            placeholder="Message"
+            required={true}
+          />
+          <label className="flex items-center gap-2">
+            <input
+              className="whitespace-break-spaces"
+              type="checkbox"
+              required={true}
+            />
+            By submitting the form, you agree to our Privacy Policy and Terms &
+            Conditions.
+          </label>
+          <input className="border border-black px-4 py-2" type="submit" />
+        </form>
+        <div className=" lg:border-r border-black lg:mt-20 lg:mb-20 lg:ml-auto" />
+        <div className="flex flex-col mt-60 lg:mr-auto lg:ml-20">
+          <span className="text-xl font-medium">Seeking a quick response?</span>
+          <span className="text-lg mt-5">Write to us:</span>
+          <span className="text-lg mt-3">Tanya Manocha</span>
+          <span className="text-lg">Founder - Creative Dugout</span>
+          <Link type="" href="mailto:@creativedugout.com" className="text-lg">
+            tanya@creativedugout.com
+          </Link>
+          <span className="text-lg mt-5">
+            Address: Building no. 90, Sector-44
+          </span>
+          <span className="text-lg">Gurugram, Haryana - 122003</span>
+        </div>
+      </div>
+    </section>
+  );
+}
+
+export default page;
+
+// dp and tree, dp on .. , learn how to explain problems, how to create edge cases,
