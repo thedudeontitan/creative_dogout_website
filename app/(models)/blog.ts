543adc26be041e8597a5f4446c9b9f5740f1ebff
Ankit Kokane <ankitkokane90@gmail.com>
2023-09-22 16:29:13 +0530

    added data for blogs

---
+++ app/(models)/blog.ts
@@ -0,0 +1,89 @@
+interface blog {
+    key: number;
+    title: string;
+    content: string;
+}
+
+export const blogs: blog[] = [
+    {
+        key: 1,
+        title: "Does your brand stand out? How you can do so?",
+        content: `How many times have you wondered what next, after you have launched a brand? The answer is almost every day because launching a brand is not enough to have a foundation for a strong brand identity. Defining brand purpose and differentiation is not merely deciding on a unique brand name or creating an innovative brand logo, these are merely the initial steps when taking a journey to develop brand identity.  
+
+        With over billions of businesses trying to stand apart from their competitors, understanding what a successful brand identity is an edge that you need to be ahead of your competition. In simple terms, brand identity is all about your brand’s personality. It’s about crafting a personality that leaves an impression on your consumers by communicating with them about your brand's core values, and products and expressing how you want people to feel when they engage with your company. 
+        
+        Here are 3 tips that can be a game changer to your brand’s visual language. 
+        
+        Crystallize your brand’s purpose
+        A brand needs to have a clear identity that showcases to the customers its unique selling proposition. This could be anything from a unique service that you offer or special perks of buying your products, but it needs to be something that clearly defines your brand's purpose and makes your brand look different from thousands of other brands that exist in the market. 
+        
+        One of the best ways to state your brand’s identity is telling the story of your brand, this helps you make a deeper connection with the consumers by letting them understand the ‘why’ behind your brand. A brand’s story should present a distinct voice that reflects the brand’s persona and not just be another strategy to increase sales since customers prefer authenticity and affinity. 
+        
+        Intimacy with customers
+        Having a strong brand purpose is closely linked to creating a loyal customer base that is based on trust, consistency and transparency. In order to gain your consumers' trust, you need to ensure that your brand delivers something that the customer needs and appreciates. Being consistent with your services and engaging in communicating with the customers can go a long way in differentiating your brand. 
+        
+        Fostering an intimate relationship with clients by making the entire experience personalized is the key to leaving a long-lasting impression. As a brand, it is essential for you to make sure that the clientele feels that their voices are heard and their opinion matters to you. One can start by answering issues that customers are facing by checking in on reviews about your products and taking the necessary steps to remedy those errors for a smooth consumer experience. 
+        
+        Connect and Advertise through digital platforms
+        With the digitalization of the world, brand differentiation is no longer confined to only having a website but it's important for all brands to create a community to drive that website. The digital world is vast from creating a blog for your brand to posting fun videos on YouTube, all can be used to create a brand with purpose. 
+        
+        The content that you post on all these platforms is detrimental to your brand’s character. It’s important to use language that matches the brand’s nature while also being emotive and connective. The plethora of social media platforms offer so many innovative ideas to advertise your products in countless different ways that will help spread the brand's message to a wider audience. 
+        
+        Apart from all this, it is crucial for every brand to track their performance and get a sense of how people interact with you simultaneously giving you the opportunity to implement changes to the brand as and when required. Thus, brand purpose and differentiation are the embodiment of what your company is and does. A solid brand identity can inspire customers and is pivotal in deciding the brand's prospects.  
+        `
+    },
+    {
+        key: 2,
+        title: "Why Brand Strategy is important for a curated Brand Culture",
+        content: `The brand culture has gradually evolved over the years, with a drastic transformation in the approach towards brand development. It has over time shifted from typography, colours, logos and websites and has evolved to an extent that they are not only responsible for providing immediate services but also ensuring that consumer experience is the top priority for every brand along with having a definitive purpose. 
+
+        Brands are no longer mere representations of companies but have progressed to become wholesome entity that is used to define aspects of consumers’ lifestyles. It’s common for companies to adjust themselves according to changing trends and customer values but it’s important to take a look back at past brand strategies in order to develop a thorough future branding strategy. 
+        
+        Progression of Brand Strategy 
+        In the earliest phase, brand strategies were limited to the visual and verbal elements of a brand. In order to have a successful product in the market, it was essential to make it look visually appealing which included experimenting with colours, logos, typography and the efficacy of the commodities upon consumption. 
+        
+        The penetration of the internet brought a dramatic change that forced brands to move beyond the visual elements. Technology ushered the need to bring transparency and consumer's voice to the forefront, giving consumers the power in brand-customer relationships. Due to the rise of the internet, brands now emphasise on providing a smooth consumer experience and exist for a purpose. 
+        
+        Businesses that have adopted the strategy to stand for something or work for a cause are better positioned in the market than others. On the other side, consumer expectations have also changed as they no longer expect simple commercial transactions but also look forward to experiences that let them indulge in the brand's aura. For real, technology did change the way that brands work. This change in optics has led brands to don the role of experience creators which has eased the shopping journey of customers. 
+        
+        Technological advancements have only gone forward, where it's possible to track consumer behaviour and preferences. The majority of brands are adopting tools that help them elevate the consumer experience even further by offering them personalised shopping journeys that are tailored to their choices. At this moment, brands are working hard to fulfil their promise by gaining insights into their target audience to ensure cultivating even stronger relationships than before. 
+        
+        Choosing a strategy that works for your brand!
+        All brands that work with the flow understand that the evolution of brand culture lies in the shift in strategy. It involves interactions that will evolve into strong intimate relationships with each consumer. All great brands embrace a policy that lets them evolve with changing trends and also makes sure that the company’s core values and message remains uncompromised while maintaining consumer expectations. 
+        
+        Every brand is different and should opt for a brand strategy that works for it to make a self–actualized culture in the future. With this, businesses will have the power to truly define and unlock their full potential creating a purer brand expression with the customer. This will be instrumental in entering a cooperative world and leaving behind the competitive environment.  
+        `
+    },
+    {
+        key: 3,
+        title: "Trust Signals: Using them to build, grow and protect your brand!",
+        content: `It’s extremely valuable, hard to give, will take years to build, and can be lost within seconds but is the building block of all relationships- Trust. It is the foundation of all relationships including all commercial transactions that millions of people make every day. 
+
+        Although technology has eased the process of shopping, businesses are more confident about selling their products without the fear of limited geographical boundaries. Trust is still fundamental because all great brands understand the importance of loyalty and transparency with their customers, but the ‘how’ behind it is something that a lot of companies find difficult to accomplish. 
+        
+        Strategic Signals are the pathway towards cultivating trust, they are elements present at various touchpoints to make customers feel more secure in their purchase journey. These signals vary across their forms but they all are present to perform the same function which is to assure security to prospective customers engaging in business with your brand. 
+        
+        Trust signals work by appealing to something that the viewer already trusts. For instance, a brand might use a certain reputable cybersecurity brand’s badge to assure users that the brand’s website is secure and reliable. By putting a reliable brand badge a lot of B2B and B2C brands borrow the credibility of others for their specific needs. 
+        
+        This was just a brief example of how trust signals work but the market is so wide that it offers trust signals in multiple forms that the majority of the brands use for their marketing. Hence, it has become essential to have knowledge about the workings of different trust signals and opt for ones that you feel are best suited for your brand’s personality. 
+        
+        Guaranteed Privacy and Security
+        Digitalization has opened avenues for a variety of new prospects but it has also made people prone to fraud which has enhanced the need for data protection and privacy. Having a professional website design will increase traffic but it's crucial that as a brand you offer people extra assurance about their privacy and protection. Trusted brands protect their customers' personal information and are transparent about the data they collect and in return deliver a personalized experience to them. 
+        
+        For example, brands can achieve this by securing websites through data protection mechanisms and adding extra layers of security. 
+        
+        Responsive customer communication
+        Imagine yourself in the position of a consumer interacting with a brand for the first time. As a first-time visitor, you would want to explore different touchpoints with the brand, out of which social media channels work more than brand promotion, services or content. It maintains a certain level of transparency to actively engage with customers and prospects. 
+        
+        Social media channels provide the opportunity to showcase to customers that they can connect on different platforms and as a brand, you can foster active audience engagement without vanishing into thin air after the purchase is made. As a result, open platforms allow brands to be accountable to customers which eventually cultivates a memorable experience for your followers.
+        
+        Customer testimonials and reviews 
+        More than 93% of purchasers today, first check out a company’s reviews and ratings and then make the decision. As a brand, you should encourage your customers to post reviews that show them that you value their experience. Positive reviews definitely help a brand augment its image as a credible and successful business in the market. 
+        
+        Concluding
+        Apart from all these major trust signals, brands can always display their achievements and endorsements which include their listed partners, or media coverage or accreditation badges to facilitate building a positive brand outlook. 
+        
+        Trust signals are a powerful way to ease your communication with users and also increase conversion rates. At the same time, it is important for all brands to note that using trust signals will only be successful when you deliver the best customer service, follow ethical business practices, work for a cause and most importantly deliver excellent services and products at the best possible price.     
+        `
+    },
+]
